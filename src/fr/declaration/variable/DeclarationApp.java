7a0b85cc9b7a390277956e504b0cfc52c936f8a3
Angie ANDURAND <angie-andurand@hotmail.fr>
2021-11-02 13:39:56 +0100

    TP3

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,19 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 10;
+		short s = -17282;
+		int i = 2846325;
+		long l = 10000L;
+		float f = 2.5F;
+		char c = 'c';
+		boolean myBool = true;
+		String str = "string \nsaut de ligne";
+		
+		System.out.println(str);
+	}
+
+}
